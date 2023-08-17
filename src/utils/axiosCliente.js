862617ed22770ff9add2d101d12f55b81a08b155
Andres <andresperlo5@gmail.com>
2023-08-17 20:05:23 -0300

    conectamos el back deployado

--- src/utils/axiosCliente.js
+++ src/utils/axiosCliente.js
@@ -3,8 +3,7 @@ import axios from 'axios'
 const token = JSON.parse(localStorage.getItem('token'))
 
 const clienteAxios = axios.create({
-  baseURL: 'http://localhost:8080/api'
-  /* baseURL: 'http://vercel.api/api' */
+  baseURL: `${import.meta.env.VITE_URL_BACK_DEPLOY}/api`
 })
 
 export const config = {
